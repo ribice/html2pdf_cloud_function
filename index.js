91e5956d50c89b912cc9fc123fe9812e1bf29320
Andrew Martin <andy123@stanford.edu>
2021-04-08 13:54:40 -0700

    swtiched to post and updated readme

--- index.js
+++ index.js
@@ -1,62 +1,55 @@
 const chromium = require("chrome-aws-lambda");
 const puppeteer = chromium.puppeteer;
-// const puppeteer = require("puppeteer-core");
 const functions = require("firebase-functions");
 
 const options = {
-  timeoutSeconds: 30
+    timeoutSeconds: 30
 };
 
 let page;
 
 async function getBrowserPage() {
-  const browser = await puppeteer.launch({
-     args: chromium.args,
-      // args: [
-      //     '--window-size=1920,1080',
-      //     '--hide-scrollbars',
-      //     '--no-sandbox'
-      // ],
-      // defaultViewport: { width: 1200, height: 1440 },
-      defaultViewport: chromium.defaultViewport,
-      executablePath: await chromium.executablePath,
-      headless: chromium.headless
-  });
-  return browser.newPage();
+    const browser = await puppeteer.launch({
+        args: chromium.args,
+        // args: [
+        //     '--window-size=1920,1080',
+        //     '--hide-scrollbars',
+        //     '--no-sandbox'
+        // ],
+        // defaultViewport: { width: 1200, height: 1440 },
+        defaultViewport: chromium.defaultViewport,
+        executablePath: await chromium.executablePath,
+        headless: chromium.headless
+    });
+    return browser.newPage();
 }
 
 exports.html2pdf = functions
-  .runWith(options)
-  .https.onRequest(async (req, res) => {
-    if (!req.originalUrl.startsWith("/?url=")) {
-      return res.status(400).send(`url query param must be provided`);
-    }
+    .runWith(options)
+    .https.onRequest(async (req, res) => {
+        let url = req.body.url;
+        if (!url) {
+            return res.status(400).send(`missing required url\n`);
+        }
+        try {
+            if (!page) {
+                page = await getBrowserPage();
+            }
 
-    let url = req.originalUrl.substring(6);
-    console.log("URL: " + url); // Do we have to escape/unescape to get QS params?
+            await page.goto(url, {waitUntil: 'networkidle2'});
+            // NOT WORKING:
+            // await page.setViewport({width: 1024, height: 1440});
+            // await page.emulateMediaType("screen");
 
-    try {
-      if (!page) {
-        page = await getBrowserPage();
-      }
+            const pdfBuffer = await page.pdf({
+                printBackground: true,
+                format: "Letter"
+            });
+            await page.close();
 
-      // console.log('headless',chromium.headless);
-      //
-      // console.log(page);
-
-      await page.goto(url, { waitUntil: 'networkidle2' });
-      await page.setViewport({ width: 1024, height: 1440 });
-      // await page.emulateMediaType("screen");
-
-      const pdfBuffer = await page.pdf({
-          printBackground: true,
-          format: "Letter"
-      });
-      await page.close();
-
-      res.set("Content-Type", "application/pdf");
-      res.status(200).send(pdfBuffer);
-    } catch (error) {
-      throw error;
-    }
-  });
+            res.set("Content-Type", "application/pdf");
+            res.status(200).send(pdfBuffer);
+        } catch (error) {
+            throw error;
+        }
+    });
